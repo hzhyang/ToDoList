ba03b01c167ce562e716fab48aba89eac6a35e05
hzhyang <zilongerdai@outlook.com>
2019-02-15 21:06:24 +0800

    build main.js

---
+++ webpack.prod.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin=require('uglifyjs-webpack-plugin');
+
+module.exports = {
+	mode: 'production', // 开发环境
+	entry: './src/index.js', // 入库
+	output: { // 出口
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		rules: [{ // react babel预编译为js
+			test: /\.(js|jsx)$/,
+			exclude: /node_modules/,
+			use: {
+				loader: 'babel-loader',
+				options: {
+					plugins: [ // babel-plugin-import antd 按需引入
+						['import', { libraryName: 'antd', style: 'css' }]  // `style: true` 会加载 less 文件
+					]
+				}}
+		},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+				// exclude: /node_modules/
+			},
+			{
+				test: /\.less$/,
+				use: ['style-loader', 'css-loader', 'less-loader'],
+				exclude: /node_modules/
+			},
+			{
+				test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
+				use:['url-loader']
+			}
+		]
+	},
+	plugins: [
+		new CleanWebpackPlugin(['dist']), // 插件 清楚dist目录
+		new HtmlWebpackPlugin({ // 将根据模板把编译好的js添加到index.html
+			title: 'todolist',
+			template: './public/index.html'
+		})
+	],
+	//压缩js
+	optimization: {
+		minimizer: [
+			new UglifyJsPlugin({
+				uglifyOptions: {
+					compress: false
+				}
+			})
+		]
+	},
+	resolve: { // 设置别名
+		alias: {
+			'@src': path.resolve(__dirname, 'src'),
+			'Components': path.resolve(__dirname, 'src/components/')
+		}
+	}
+}
